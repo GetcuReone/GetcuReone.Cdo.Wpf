fae366cf0ac9ffcb774673769268455fdd7dae8b
Juel <GetcuReone@hotmail.com>
2021-02-06 04:12:04 +0300

    Update settings for transmission of navigation information Fixes #5

--- GetcuReone.Cdo.Wpf/GetcuReone.Cdo.WpfTests/Env/UiTestBase.cs
+++ GetcuReone.Cdo.Wpf/GetcuReone.Cdo.WpfTests/Env/UiTestBase.cs
@@ -1,14 +1,6 @@
-﻿using GetcuReone.Cdi;
-using GetcuReone.Cdo.File;
-using GetcuReone.Cdo.Helpers;
-using GetcuReone.ComboPatterns.Adapter;
+﻿using GetcuReone.ComboPatterns.Adapter;
 using GetcuReone.ComboPatterns.Interfaces;
-using GetcuReone.GetcuTestAdapter.Entities;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
 
 namespace GetcuReone.Cdo.WpfTests.Env
 {
@@ -22,27 +14,6 @@ namespace GetcuReone.Cdo.WpfTests.Env
         public TAdapter GetAdapter<TAdapter>() where TAdapter : IAdapter, new()
         {
             return AdapterBase.Create<TAdapter>(this);
-        }
-
-        [TestInitialize]
-        public virtual void Initialize()
-        {
-            using (var fileStream = GetAdapter<FileAdapter>().OpenRead(@"Env\config.xml"))
-            {
-                var configValue = fileStream.DeserializeFromXml<List<ConfigValue>>();
-                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-                var settings = config.AppSettings.Settings;
-
-                foreach (var value in configValue)
-                {
-                    if (!settings.AllKeys.IsNullOrEmpty() && settings.AllKeys.Any(key => key.EqualsOrdinal(value.Key)))
-                        settings.Remove(value.Key);
-
-                    settings.Add(value.Key, value.Value);
-                }
-
-                config.Save(ConfigurationSaveMode.Modified);
-            }
         }
     }
 }
