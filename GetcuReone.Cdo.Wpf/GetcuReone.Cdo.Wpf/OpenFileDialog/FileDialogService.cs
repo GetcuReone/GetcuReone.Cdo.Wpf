93c954f6f70e4b6e3acdeff121fc6d7aab85054b
Juel <GetcuReone@hotmail.com>
2021-03-04 18:03:38 +0300

    You can't mock the OpenFileDialogAdapter class Fixes #20

---
+++ GetcuReone.Cdo.Wpf/GetcuReone.Cdo.Wpf/OpenFileDialog/FileDialogService.cs
@@ -0,0 +1,27 @@
+﻿using GetcuReone.Cdo.Wpf.OpenFileDialog.Entities;
+using System.Linq;
+
+namespace GetcuReone.Cdo.Wpf.OpenFileDialog
+{
+    internal sealed class FileDialogService : IFileDialog
+    {
+        public SelectFilesResult SelectFiles(SelectFilesRequest request)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+
+            dialog.Filter = request.Filter;
+            if (string.IsNullOrEmpty(request.TitleDialog))
+                dialog.Title = request.TitleDialog;
+            if (string.IsNullOrEmpty(request.InitialFolder))
+                dialog.InitialDirectory = request.InitialFolder;
+            dialog.CheckFileExists = request.CheckFileExists;
+            dialog.Multiselect = request.MultiSelect;
+
+            return new SelectFilesResult
+            {
+                ResultShowDialog = dialog.ShowDialog(),
+                SelectedFiles = dialog.FileNames.ToList(),
+            };
+        }
+    }
+}
---
+++ GetcuReone.Cdo.Wpf/GetcuReone.Cdo.Wpf/OpenFileDialog/IFileDialog.cs
@@ -0,0 +1,17 @@
+﻿using GetcuReone.Cdo.Wpf.OpenFileDialog.Entities;
+
+namespace GetcuReone.Cdo.Wpf.OpenFileDialog
+{
+    /// <summary>
+    /// Service for work FileExplorer
+    /// </summary>
+    public interface IFileDialog
+    {
+        /// <summary>
+        /// Open file explorer to select files.
+        /// </summary>
+        /// <param name="request">Request.</param>
+        /// <returns></returns>
+        SelectFilesResult SelectFiles(SelectFilesRequest request);
+    }
+}
